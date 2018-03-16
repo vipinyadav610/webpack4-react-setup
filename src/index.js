0a3a8860dbbed08c95face3da1808b2c3775ee7d
vipinyadav610 <adi19910810@gmail.com>
2018-03-17 00:32:12 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(<div>title</div>, document.getElementById("app"));
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+module.exports = {
+  entry: ["react-hot-loader/patch", "./src/index.js"],
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: ["./dist", "./public"],
+    historyApiFallback: true,
+    // host: "0.0.0.0",
+    port: 3000,
+    hot: true,
+    https: true,
+    open: true,
+    progress: true
+    // watchContentBase: true
+  }
+};
