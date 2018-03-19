ac9770570af432fb3f897d25783d48f9826de18f
Vipin yadav <vipin.yadav@asticsinc.com>
2018-03-19 20:03:05 +0530

    linting rules defined

--- src/client/redux/middlewares/promise.js
+++ src/client/redux/middlewares/promise.js
@@ -1,6 +1,7 @@
+import cookies from "js-cookies";
 const promiseMiddleware = () => {
   return next => action => {
-    const { promise, type, ...rest } = action;
+    const { promise, type, token, ...rest } = action;
 
     if (!promise) return next(action);
 
@@ -12,11 +13,25 @@ const promiseMiddleware = () => {
       .then(response => {
         next({ ...rest, response: response.data, type: SUCCESS });
         return true;
+        if (token) {
+          const secure = window.location.protocol === "https";
+          cookies.setItem(
+            "API_TOKEN",
+            response.data.token,
+            undefined,
+            "/",
+            undefined,
+            secure
+          );
+        }
       })
       .catch(error => {
         next({ ...rest, error, type: FAILURE });
         console.log(error);
         return false;
+        if (error.status.code == 401) {
+          cookies.remove("API_TOKEN");
+        }
       });
   };
 };
