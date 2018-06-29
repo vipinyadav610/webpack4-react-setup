c395fe9e757714b6a82e56f1f4e79be2bac949da
Vipin yadav <vipin.yadav@asticsinc.com>
2018-06-29 16:38:02 +0530

    server auth modules added

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -2,65 +2,61 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import * as Actions from '../../redux/actions/posts'
-import { Query, ApolloConsumer, graphql } from 'react-apollo'
+import { Query, ApolloConsumer } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const query = gql`
-  query todo($itemId: Int!) {
-    todo(itemId: $itemId) {
-      itemId
-      item
-    }
-  }
-`
-// const variables = { itemId: 1 }
-
 @connect(state => ({}), Actions)
-@graphql(query, {
-  options: ownProps => ({
-    variables: {
-      itemId: 1
-    }
-  })
-})
+// @graphql(query, {
+//   options: ownProps => ({
+//     variables: {
+//       itemId: 1
+//     }
+//   })
+// })
 class Home extends React.Component {
+  query = () => {
+    return gql`
+      query todo($itemId: Int!) {
+        todo(itemId: $itemId) {
+          itemId
+          item
+        }
+      }
+    `
+  }
+  variables = () => {
+    1
+  }
   render() {
-    console.log('this.props', this.props.data)
-    return 'hello'
-    // return (
-    //   <Query query={query} variables={variables}>
-    //     {({ loading, error, data }) => {
-    //       console.log('data', data)
-    //       if (loading) return <p>Loading...</p>
-    //       if (error) return <p>Error :(</p>
-    //       return (
-    //         <div>
-    //           <h1>hello world</h1>
-    //           <ApolloConsumer>
-    //             {client => (
-    //               <button
-    //                 onClick={async () => {
-    //                   const { data } = await client.query({
-    //                     query,
-    //                     variables
-    //                   })
-    //                   console.log('data', data)
-    //                 }}
-    //               >
-    //                 Click me!
-    //               </button>
-    //             )}
-    //           </ApolloConsumer>
-    //         </div>
-    //       )
-    //       // return data.rates.map(({ currency, rate }) => (
-    //       //   <div key={currency}>
-    //       //     <p>{`${currency}: ${rate}`}</p>
-    //       //   </div>
-    //       // ))
-    //     }}
-    //   </Query>
-    // )
+    return (
+      <Query query={this.query} variables={this.variables}>
+        {({ loading, error, data }) => {
+          console.log('data', data)
+          if (loading) return <p>Loading...</p>
+          if (error) return <p>Error :(</p>
+          return (
+            <div>
+              <h1>hello world</h1>
+              <ApolloConsumer>
+                {client => (
+                  <button
+                    onClick={async () => {
+                      const { data } = await client.query({
+                        query,
+                        variables
+                      })
+                      console.log('data', data)
+                    }}
+                  >
+                    Click me!
+                  </button>
+                )}
+              </ApolloConsumer>
+            </div>
+          )
+        }}
+      </Query>
+    )
   }
 }
 Home.propTypes = {
