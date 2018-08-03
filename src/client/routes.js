467b75631b3e5c544ea03009435130fce8ea46f2
Vipin Yadav <vipin.yadav@asticsinc.com>
2018-08-03 14:15:47 +0530

    lint

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,9 +1,9 @@
 import asyncComponent from './hoc/asyncRender'
 import ProtectedRoute from './hoc/auth'
 
-// const Home = asyncComponent(() =>
-//   import('./components/Home').then(module => module.default)
-// )
+const Home = asyncComponent(() =>
+  import('./components/Home').then(module => module.default)
+)
 const NotFound = asyncComponent(() =>
   import('./components/NotFound').then(module => module.default)
 )
